70db24ef89a6656627f16ae9e9e25c67219018ff
vbnm0 <vbnm0149@gmail.com>
2023-07-27 20:35:32 -0400

    j

--- rng/main.js
+++ rng/main.js
@@ -1,6 +1,15 @@
 function roll() {
     let d = document.getElementById("dice");
+    let t = document.getElementById("dtxt");
     let x = Math.floor(Math.random() * 6 + 1);
     let s = "https://www.vbnm.dev/images/dice/" + x + ".png";
     d.src = s;
+    t.innerHTML = x;
+    return x;
+}
+
+function rollc() {
+    for (i = 0; i < 10; i++) {
+        roll();
+    }
 }
