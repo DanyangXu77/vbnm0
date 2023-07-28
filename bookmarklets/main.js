f30d54d210d666aa11b7de117ffb9c718a5d71cd
vbnm0 <vbnm0149@gmail.com>
2023-07-27 20:31:11 -0400

    e

--- bookmarklets/main.js
+++
@@ -1,6 +0,0 @@
-function myFunction(a) {
-    var copyText = document.getElementById(a);
-    copyText.select();
-    copyText.setSelectionRange(0, 99999);
-    navigator.clipboard.writeText(copyText.value);
-}
