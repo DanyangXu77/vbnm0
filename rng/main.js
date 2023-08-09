b69a56a0670f708d93c28e564ccc35a16f11cf30
vbnm0 <vbnm0149@gmail.com>
2023-08-08 22:25:18 -0400

    card but disp

--- rng/main.js
+++ rng/main.js
@@ -2,6 +2,7 @@ let cards = Array.apply(null, Array(54)).map(function (x, i) { return i; });
 let oneDeck = Array.apply(null, Array(54)).map(function (x, i) { return i; });
 let shownCards = [];
 let cardDeck = document.getElementById("cardDeck");
+let topCard = document.getElementById("cardDisp");
 
 function roll() {
     let d = document.getElementById("dice");
@@ -17,7 +18,7 @@ function reset() {
     cards = Array.apply(null, Array(54)).map(function (x, i) { return i; });
     oneDeck = Array.apply(null, Array(54)).map(function (x, i) { return i; });
     shownCards = [];
-    cardDeck.height = 9;
+    cardDeck.height = 54;
 }
 
 function shuffle() {
@@ -34,10 +35,52 @@ function shuffle() {
 
 function addDeck() {
     cards = cards.concat(oneDeck);
-    cardDeck.height = cards.length / 6;
+    cardDeck.height = cards.length;
 }
 
 function draw() {
-    shownCards.push(cards.pop());
-    cardDeck.height = cards.length / 6;
+    card = cards.pop();
+    shownCards.push(card);
+    topCard.innerHTML = cardName(card);
+    cardDeck.height = cards.length;
+}
+
+function cardName(num) {
+    if (num == 52) {
+        return "Small Joker";
+    }
+    if (num == 53) {
+        return "Big Joker";
+    }
+    let ret = "";
+    let num = num % 13;
+    let suit = Math.floor(num / 13);
+    if (num == 0) {
+        ret += "Ace of ";
+    }
+    if (num < 10 && num > 0) {
+        ret += num + 1 + "of ";
+    }
+    if (num == 10) {
+        ret += "Jack of "
+    }
+    if (num == 11) {
+        ret += "Queen of "
+    }
+    if (num == 12) {
+        ret += "King of "
+    }
+
+    if (suit == 0) {
+        ret += "Spades";
+    }
+    if (suit == 1) {
+        ret += "Hearts";
+    }
+    if (suit == 2) {
+        ret += "Clubs";
+    }
+    if (suit == 3) {
+        ret += "Diamonds";
+    }
 }
