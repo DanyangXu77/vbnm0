819b00521c3e675d3319fe31f599d8242fbb255a
Violet <vbnm0149@gmail.com>
2023-08-24 20:13:44 -0400

    test

--- randombs/detect/main.js
+++ randombs/detect/main.js
@@ -1,2 +1,3 @@
 document.getElementById("pageblock").hidden = true;
 document.getElementById("dispAgent").innerHTML = navigator.userAgent;
+document.getElementById("dispInfo").innerHTML = navigator.getBattery().level;
