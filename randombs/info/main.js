f7e2ace14226dc7f38622e01aba93774e763c4b0
Violet <vbnm0149@gmail.com>
2023-08-28 18:07:46 -0400

    test

--- randombs/info/main.js
+++ randombs/info/main.js
@@ -1,16 +1,16 @@
 document.getElementById("pageblock").hidden = true;
 document.getElementById("dispAgent").innerHTML = navigator.userAgent;
 updateBattery();
-let x = setTimeout(updateBattery, 1000);
+let x = setInterval(updateBattery, 1000);
 let updates = 0;
 
 
 async function updateBattery() {
     updates++;
-    console.log(updates);
+    console.log("Battery Update #" + updates);
     if (!navigator.getBattery) {
         document.getElementById("batteryInfo").innerHTML = "Battery Info <br>unknown due to either a permissions policy or a being called from an insecure context.";
-        clearTimeout(x);
+        clearInterval(x);
     }
     let battery = await navigator.getBattery();
     document.getElementById("batteryLvl").innerHTML = "Level: " + Math.round((battery.level * 100)) + "%";
