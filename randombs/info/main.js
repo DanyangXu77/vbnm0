09adeb25c4ca4a9a03afb19bcd30b291b3377dcd
Violet <vbnm0149@gmail.com>
2023-08-29 16:55:18 -0400

    test

--- randombs/info/main.js
+++ randombs/info/main.js
@@ -38,9 +38,9 @@ navigator.getBattery().then((battery) => {
 });
 
 navigator.connection.addEventListener('change', function(network) {
-    document.getElementById("bandwidth") = "Bandwidth: " + network.downlink + " Kbps";
-    document.getElementById("maxBandwidth") = "Max Bandwidth: " + network.downlinkMax + " Mbps";
-    document.getElementById("effectiveType") = "Effective Type: " + network.effectiveType;
-    document.getElementById("roundTripTime") = "Round Trip Time:" + network.rtt + " Milliseconds";
-    document.getElementById("connectionType").innerHTML = "Connection Type: " + network.type.charAt(0).toString().toUpperCase() + network.type.toString().splice(1);
+    document.getElementById("bandwidth").innerHTML = "Bandwidth: " + network.downlink == undefined ? "Unknown" : network.downlink + " Mbps";
+    document.getElementById("maxBandwidth").innerHTML = "Max Bandwidth: " + network.downlinkMax == undefined ? "Unknown" : network.downlinkMax + " Mbps";
+    document.getElementById("effectiveType").innerHTML = "Effective Type: " + network.effectiveType == undefined ? "Unknown" : network.effectiveType;
+    document.getElementById("roundTripTime").innerHTML = "Round Trip Time: " + network.rtt == undefined ? "Unknown" : network.rtt + " Milliseconds";
+    document.getElementById("connectionType").innerHTML = "Connection Type: " + network.type == undefined ? "Unknown" : network.type.charAt(0).toUpperCase() + network.type.splice(1);
 });
